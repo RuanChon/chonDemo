4cc9b4201783006ebceb2b29b656a781ebda9c19
chon <724082712@qq.com>
2022-10-27 23:40:27 +0800

    feat: 环境变量

---
+++ e_vue/env_path/src/App.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: Chon
+ * @Date: 2022-10-27 23:03:49
+ * @LastEditors: chon 724082712@qq.com
+ * @LastEditTime: 2022-10-27 23:17:40
+ * @FilePath: \my_study_vlog\e_vue\env_path\src\App.vue
+ * @Description: 文件说明
+-->
+<template>
+  <div>app</div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue"
+
+onMounted(() => {
+  console.log(import.meta.env)
+})
+</script>
+
+<style scoped></style>
