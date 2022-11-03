34e8706cf58c5f2dbace2cee88d525171bd92972
chon <724082712@qq.com>
2022-11-03 18:59:26 +0800

    feat:

---
+++ e_vue/router-demo/src/router/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: Chon
+ * @Date: 2022-11-03 15:08:46
+ * @LastEditors: chon 724082712@qq.com
+ * @LastEditTime: 2022-11-03 18:59:17
+ * @FilePath: \my_study_vlog\e_vue\router-demo\src\router\index.ts
+ * @Description: 文件说明
+ */
+import { createRouter } from "vue-router"
