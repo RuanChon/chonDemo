699faf0e90216496c43f1f334f1caa0724b5c428
chon <724082712@qq.com>
2022-11-08 23:32:39 +0800

    fix:

--- e_vue/router-demo/src/main.ts
+++ e_vue/router-demo/src/main.ts
@@ -1,9 +1,6 @@
 /*
  * @Author: Chon
- * @Date: 2022-11-01 16:41:48
- * @LastEditors: chon 724082712@qq.com
- * @LastEditTime: 2022-11-04 23:30:55
- * @FilePath: \my_study_vlog\e_vue\router-demo\src\main.ts
+ * @Date: 2022-11-08 23:30:24
  * @Description: 文件说明
  */
 import { createApp } from "vue"
