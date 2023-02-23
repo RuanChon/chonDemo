7c915d5716f3e16a5745b5fbff79dd6fa9756048
chon <724082712@qq.com>
2023-02-23 17:41:53 +0800

    feat: sleep

--- _demo/sleep.js
+++ _demo/sleep.js
@@ -6,11 +6,13 @@
 // 使用 Promise 包着 setTimeout，并使用箭头函数返回
 const sleep = delay => new Promise(resolve => setTimeout(resolve, delay))
 
+var num = 1
+
+devSleep()
+
 async function devSleep() {
   // 需要和 await 配合使用
   await sleep(2000)
-
-  console.log("sleep log")
+  console.log("sleep log", num)
+  var num = 2
 }
-
-devSleep()
