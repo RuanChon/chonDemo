67c2b45db00d8b9c6627456dcf352f11b4433805
chon <724082712@qq.com>
2023-02-28 22:27:34 +0800

    feat:

--- _demo/hello.ts
+++ _demo/hello.ts
@@ -4,3 +4,24 @@
  * @Description: 文件说明
  */
 let str: string = "1212"
+
+let num: Object = [1, "2", {}]
+
+function fun1(...args: any[]): void {
+  args[0] = 3
+  console.log(args)
+  let str: string = args[0]
+  str += 1
+  console.log(str)
+}
+
+fun1(1, "2")
+
+function fn(arg1: number): void
+function fn(arg1: string, arg2: number): void
+function fn(arg1: any, arg2?: any): void {
+  console.log(arg1)
+  console.log(arg2)
+}
+
+fn("1", 2)
