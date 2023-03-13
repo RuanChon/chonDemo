c18d35f220db6a80e711b9021c6cd907b51ba7b8
chon <724082712@qq.com>
2023-03-13 23:57:42 +0800

    feat: enum

---
+++ _demo/enum.js
@@ -0,0 +1,7 @@
+var Types;
+(function (Types) {
+    Types[Types["\u5F85\u53D1\u8D27"] = 1] = "\u5F85\u53D1\u8D27";
+    Types[Types["\u5DF2\u53D1\u8D27"] = 2] = "\u5DF2\u53D1\u8D27";
+    Types[Types["\u672A\u53D1\u8D27"] = 3] = "\u672A\u53D1\u8D27";
+})(Types || (Types = {}));
+console.log("当前状态为", Types[3]);
--- _demo/hello.js
+++ _demo/hello.js
@@ -5,3 +5,17 @@
  * @Description: 文件说明
  */
 let str = "1212";
+let num = [1, "2", {}];
+function fun1(...args) {
+    args[0] = 3;
+    console.log(args);
+    let str = args[0];
+    str += 1;
+    console.log(str);
+}
+fun1(1, "2");
+function fn(arg1, arg2) {
+    console.log(arg1);
+    console.log(arg2);
+}
+fn("1", 2);
