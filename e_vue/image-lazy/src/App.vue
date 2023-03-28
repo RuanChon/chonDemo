629cc421f126bb3e2ac55d607b7cadfe85d6c4ba
chon <724082712@qq.com>
2023-03-28 11:27:14 +0800

    feat: v-lazy

--- e_vue/image-lazy/src/App.vue
+++ e_vue/image-lazy/src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <h3>图片懒加载</h3>
+    <HelleWorld :tips="tips" @setTitle="getTitle" />
+    <h3>{{ title }}</h3>
     <img v-lazy="item" v-for="item in imgArr" />
   </div>
 </template>
 <script setup lang="ts">
+import { ref } from "vue"
 import type { Directive } from "vue"
+import HelleWorld from "./components/HelloWorld.vue"
+
+const tips = "请在开发者工具调整为移动端查看"
+const title = ref()
+const getTitle = (val: string) => {
+  title.value = val
+}
+
 // vite 提供的导入文件夹所有文件
 // glob 是懒加载的模式
 // let module = {
@@ -19,18 +29,18 @@ const imgArr = Object.values(imgLists).map(v => v.default)
 console.log("imglists", imgArr)
 
 // 懒加载自定义指令
-let vLazy: Directive<HTMLImageElement, string> = async (el, bingding) => {
-  console.log("21", bingding)
+let vLazy: Directive<HTMLImageElement, string> = async (el, imgData) => {
+  // console.log("21", imgData)
   const logo = await import("./assets/vue.svg")
   el.src = logo.default
   // 最重要的一步，判断图片是否在可视区域内
   // IntersectionObserver API 回调里的 options参数是个数组，其中数组内第一个对象里有个 intersectionRatio 属性
   // intersectionRatio 用来判断图片是否在可视区域内，当大于 0 时则表示出现在可视区域内
   const observer = new IntersectionObserver(options => {
-    console.log("options", options[0])
+    // console.log("options", options[0])
     setTimeout(() => {
       if (options[0].intersectionRatio > 0) {
-        el.src = bingding.value
+        el.src = imgData.value
         // 取消监听
         observer.unobserve(el)
       }
--- e_vue/image-lazy/src/components/HelloWorld.vue
+++ e_vue/image-lazy/src/components/HelloWorld.vue
@@ -1,38 +1,25 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div class="card">{{ tips }}</div>
 </template>
 
+<script setup lang="ts">
+import { defineProps, onMounted, useAttrs } from "vue"
+
+const emits = defineEmits(["setTitle"])
+defineProps({
+  tips: String,
+})
+
+const attrs = useAttrs()
+const setTitle = "图片懒加载"
+
+onMounted(() => {
+  emits("setTitle", setTitle)
+})
+</script>
+
 <style scoped>
-.read-the-docs {
-  color: #888;
+.card {
+  line-height: 50px;
 }
 </style>
